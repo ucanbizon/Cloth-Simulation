f69e6a7f80f24129f6a0e5879f7c9ada6f9dd143
Kutay Macit <ucanbizon@gmail.com>
2018-08-18 20:33:25 -0500

    Add files via upload

---
+++ Fast Simulation of Mass-Spring Systems/Cloth.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <Eigen/Sparse>
+#include "FastBox.h"
+#include "Plane.h"
+#include "Sphere.h"
+class Cloth {
+friend class Renderer;
+friend class FastBox;
+public:
+	Cloth(unsigned int nNodes = 80, float nodeMass = 1.0f, float shearStretchK = 400.0f, float bendK = 80.0f, float timeStep = 0.04f);
+	void Run(unsigned int iterations = 8);
+	void ComputeNormals();
+	unsigned int GetSNodeCount() const { return snodeCount; }
+	void SphereCollision(FastBox& fastbox, Sphere& sphere);
+	void PlaneCollision(FastBox& fastbox, Plane& ground);
+	void SelfCollision(FastBox& fastbox);
+	void SetExternalForce();
+private:
+	void NodeInit();
+	void SpringInit();
+	void LMatrixBuild(float shearStretchK, float bendK);
+	void JMatrixBuild(float shearStretchK, float bendK);
+	void MMatrixBuild(float nodeMass);
+	void Mph2LMatrixBuild();
+	
+	inline void LocalStep();
+	inline void GlobalStep();
+	void IndicesInit();
+	bool TrianglePointProj(unsigned int triInd, unsigned int nodeInd, float& u, float& v);
+	bool EdgeProj(unsigned int p0, unsigned int p1, unsigned int q0, unsigned int q1);
+
+	float nodeMass;
+	float timeStep;
+	unsigned int snodeCount;
+	unsigned int nodeCount;
+	unsigned int springCount;
+	Eigen::VectorXf nodes;
+	Eigen::VectorXf previousNodes;
+	Eigen::VectorXi springs;
+	Eigen::VectorXf springsRestLength;
+	Eigen::VectorXf springsDirection;
+	Eigen::VectorXf y;
+	Eigen::VectorXf fext;
+	Eigen::SparseMatrix<int> triPointConstraints;
+
+	Eigen::SparseMatrix<float> lMatrix;
+	Eigen::SparseMatrix<float> jMatrix;
+	Eigen::SparseMatrix<float> mMatrix;
+	Eigen::SimplicialLLT<Eigen::SparseMatrix<float> > Mph2L;
+
+	//for rendering
+	Eigen::VectorXi indices;
+	Eigen::VectorXf normals;
+
+};
---
+++ Fast Simulation of Mass-Spring Systems/main.cpp
@@ -0,0 +1,29 @@
+#include "Cloth.h"
+#include "FastBox.h"
+#include "Sphere.h"
+#include "Plane.h"
+#include "Renderer.h"
+
+int main() {
+
+	Cloth cloth(20, 1.0f, 100.0f, 80.0f);
+	FastBox fastbox(cloth);
+	Sphere sphere(Eigen::Vector3f(0.0, -50.0,0.0), 50.0f);
+	Plane ground(-35.0f);
+	Renderer renderer(800, 600, cloth, sphere, ground);
+	while (!glfwWindowShouldClose(renderer.GetWindow())){
+
+		cloth.Run(20);
+		fastbox.PlaneBoxCollision(cloth, ground, true);
+		cloth.PlaneCollision(fastbox, ground);
+		fastbox.SphereBoxCollision(cloth, sphere, true);
+		cloth.SphereCollision(fastbox, sphere);
+		fastbox.SelfBoxCollision(cloth);
+		cloth.SelfCollision(fastbox);
+		cloth.ComputeNormals();
+		renderer.Render(cloth, sphere, ground);
+	}
+	return 0;
+}
+
+
